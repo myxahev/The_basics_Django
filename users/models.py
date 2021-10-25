0f1ad73ffce7bf58b9e47596f4dbeb6b6836046a
Myxahev <myxahev2@gmail.com>
2021-10-25 10:32:14 +0500

    Complete task

--- users/models.py
+++ users/models.py
@@ -7,4 +7,3 @@ class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True, null=True)
     middle_name = models.CharField('Отчество', max_length=150, blank=True, null=True)
     age = models.PositiveIntegerField(verbose_name='возраст', blank=True, null=True)
-
--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from users.views import login, registration, logout
+from users.views import login, registration, logout, profile
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('registration/', registration, name='registration'),
-    path('logout/', logout, name='logout')
+    path('profile/', profile, name='profile'),
+    path('logout/', logout, name='logout'),
+
 ]
