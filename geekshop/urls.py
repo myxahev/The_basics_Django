6978de89859e7c11ece0a93044fca026c8453227
Dmitry <myxahev2@gmail.com>
2021-10-31 15:22:04 +0500

    add models admin

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('products/', include('products.urls', namespace='products')),
     path('users/', include('users.urls', namespace='users')),
     path('baskets/', include('baskets.urls', namespace='baskets')),
+    path('admins/', include('admins.urls', namespace='admins')),
 
 ]
 
