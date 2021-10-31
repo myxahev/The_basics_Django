1ed45f61aed1246ec25576b1b47322fc4fdcedb5
Dmitry <myxahev2@gmail.com>
2021-10-31 15:52:34 +0500

    coplete task_7

--- products/admin.py
+++ products/admin.py
@@ -3,4 +3,11 @@ from products.models import Product, ProductCategory
 
 # Register your models here.
 admin.site.register(ProductCategory)
-admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('name', 'image', 'description', ('price', 'quantity'), 'category')
+    readonly_fields = ('description',)
+    ordering = ('name',)
+    search_fields = ('name',)
