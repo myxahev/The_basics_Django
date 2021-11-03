3e8d34dcef278ac9d7aa0c5e82f5de7c3d2b0189
Dmitry <myxahev2@gmail.com>
2021-11-03 15:54:07 +0500

    complete HW_8

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+
 from products.views import products
 
 app_name = 'products'
 
 urlpatterns = [
-    path('', products, name='index')
+    path('', products, name='index'), # .../products/
+    path('<int:category_id>/', products, name='category'), # ../products/<category_id>/
+    path('page/<int:page>/', products, name='page'), # ../products/page/<page_num>/
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,14 @@
+
 from django.urls import path
 
-from users.views import login, registration, logout, profile
+from users.views import login, logout, profile
+from users.views import UserRegistrationView
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', login, name='login'),
-    path('registration/', registration, name='registration'),
+    path('registration/', UserRegistrationView.as_view(), name='registration'),
     path('profile/', profile, name='profile'),
     path('logout/', logout, name='logout'),
 
