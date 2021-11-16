307380bba4e107fdaa4fe776bcc8a768d9547bd0
Dmitry <myxahev2@gmail.com>
2021-11-16 22:02:00 +0500

    add vk

---
+++ products/management/commands/update_db.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+
+
+from users.models import User, UserProfile
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        users = User.objects.all()
+        for user in users:
+            user_profile = UserProfile.objects.create(user=user)
+            user_profile.save()
