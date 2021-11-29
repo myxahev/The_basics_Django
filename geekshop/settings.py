9515abbb9eb57509a79c4317a1d7b5dc92136deb
Dmitry <myxahev2@gmail.com>
2021-11-29 19:02:18 +0500

    fix error 0.2

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -90,9 +90,14 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+    'default': {
+        'NAME': 'geekshop',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
      }
 }
 
